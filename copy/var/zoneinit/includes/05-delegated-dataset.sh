9951ae3e67eabf9d2099712d60151ca5666d5c70
Thomas Merkel <tm@core.io>
2015-11-09 15:49:07 +0100

    Add delegate dataset script and nginx start script

---
+++ copy/var/zoneinit/includes/05-delegated-dataset.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+UUID=$(mdata-get sdc:uuid)
+DDS=zones/${UUID}/data
+
+if zfs list ${DDS} 1>/dev/null 2>&1; then
+	zfs create ${DDS}/gogs-custom       || true
+	zfs create ${DDS}/gogs-data         || true
+	zfs create ${DDS}/gogs-repositories || true
+
+	zfs set mountpoint=/opt/gogs/custom ${DDS}/gogs-custom
+	zfs set mountpoint=/opt/gogs/data ${DDS}/gogs-data
+	zfs set mountpoint=/opt/gogs/gogs-repositories ${DDS}/gogs-repositories
+fi
+
+# znapzend for backup
+znapzendzetup create --recursive --tsformat='%Y-%m-%d-%H%M%S' --donotask \
+	SRC '7day=>8hour,30day=>1day,1year=>1week,10year=>1month' ${DDS}/gogs-custom
+znapzendzetup create --recursive --tsformat='%Y-%m-%d-%H%M%S' --donotask \
+	SRC '7day=>8hour,30day=>1day,1year=>1week,10year=>1month' ${DDS}/gogs-data
+znapzendzetup create --recursive --tsformat='%Y-%m-%d-%H%M%S' --donotask \
+	SRC '7day=>8hour,30day=>1day,1year=>1week,10year=>1month' ${DDS}/gogs-repositories
+/usr/sbin/svcadm enable svc:/pkgsrc/znapzend:default
---
+++ copy/var/zoneinit/includes/24-nginx.sh
@@ -0,0 +1,1 @@
+svcadm enable svc:/pkgsrc/nginx:default
