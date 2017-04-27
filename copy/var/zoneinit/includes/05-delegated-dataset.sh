9f7ce1c30daadf6674edb8fce776ffe38adaba2d
Thomas Merkel <tm@core.io>
2017-04-27 10:38:01 +0200

    Create delegate dataset for /opt/gogs/.ssh folder

--- copy/var/zoneinit/includes/05-delegated-dataset.sh
+++ copy/var/zoneinit/includes/05-delegated-dataset.sh
@@ -6,10 +6,12 @@ if zfs list ${DDS} 1>/dev/null 2>&1; then
 	zfs create ${DDS}/gogs-custom       || true
 	zfs create ${DDS}/gogs-data         || true
 	zfs create ${DDS}/gogs-repositories || true
+	zfs create ${DDS}/gogs-ssh          || true
 
 	zfs set mountpoint=/opt/gogs/custom ${DDS}/gogs-custom
 	zfs set mountpoint=/opt/gogs/data ${DDS}/gogs-data
 	zfs set mountpoint=/opt/gogs/gogs-repositories ${DDS}/gogs-repositories
+	zfs set mountpoint=/opt/gogs/.ssh
 
 	# znapzend for backup
 	znapzendzetup create --recursive --tsformat='%Y-%m-%d-%H%M%S' --donotask \
@@ -20,10 +22,11 @@ if zfs list ${DDS} 1>/dev/null 2>&1; then
 		SRC '7day=>8hour,30day=>1day,1year=>1week,10year=>1month' ${DDS}/gogs-repositories
 	/usr/sbin/svcadm enable svc:/pkgsrc/znapzend:default
 else
-	mkdir /opt/gogs/{custom,data,gogs-repositories}
+	mkdir /opt/gogs/{custom,data,gogs-repositories,.ssh}
 fi
 
 # update permissions for git usage
 chown git:other /opt/gogs/custom
 chown git:other /opt/gogs/data
 chown git:other /opt/gogs/gogs-repositories
+chown git:other /opt/gogs/.ssh
--- copy/var/zoneinit/includes/25-gogs.sh
+++ copy/var/zoneinit/includes/25-gogs.sh
@@ -1,3 +1,6 @@
-# Enable gogs service
+# Paste default environment file
+echo "PATH=/usr/local/sbin:/usr/local/bin:/opt/local/sbin:/opt/local/bin:/usr/sbin:/usr/bin:/sbin" > \
+	/opt/gogs/.ssh/environment
 
+# Enable gogs service
 svcadm enable svc:/application/gogs:default
