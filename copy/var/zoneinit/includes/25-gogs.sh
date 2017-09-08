879457fcd06daec815b9614e60218709b7ede64f
Thomas Merkel <tm@core.io>
2017-09-08 15:43:30 +0200

    Fix PermitUserEnvironment for gogs

--- copy/var/zoneinit/includes/25-gogs.sh
+++ copy/var/zoneinit/includes/25-gogs.sh
@@ -2,5 +2,9 @@
 echo "PATH=/usr/local/sbin:/usr/local/bin:/opt/local/sbin:/opt/local/bin:/usr/sbin:/usr/bin:/sbin" > \
 	/opt/gogs/.ssh/environment
 
+# Enable environment configuration for users
+sed -e 's:.*PermitUserEnvironment.*no:PermitUserEnvironment yes:g' \
+	/etc/ssh/sshd_config
+
 # Enable gogs service
 svcadm enable svc:/application/gogs:default
