4d2b88d6e5cb5cc2aab28d3803afb2887ec69a12
Thomas Merkel <tm@core.io>
2019-04-25 12:13:23 +0200

    Fix ssl-generator script path

--- copy/var/zoneinit/includes/46-nginx.sh
+++ copy/var/zoneinit/includes/46-nginx.sh
@@ -2,7 +2,7 @@
 svcadm enable svc:/pkgsrc/nginx:default
 
 # Configure SSL
-/opt/core/bin/ssl-generate.sh /opt/local/etc/nginx/ssl nginx_ssl nginx svc:/pkgsrc/nginx:default
+/opt/core/bin/ssl-generator.sh /opt/local/etc/nginx/ssl nginx_ssl nginx svc:/pkgsrc/nginx:default
 
 # Restart nginx to support new SSL certificate
 svcadm restart svc:/pkgsrc/nginx:default
