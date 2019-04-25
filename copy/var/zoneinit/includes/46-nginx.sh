5c45dd2a32587420cd38081b64143c2d40add589
Thomas Merkel <tm@core.io>
2019-04-25 11:36:33 +0200

    core-base and gogs version bump to 0.11.88.0220

--- copy/var/zoneinit/includes/46-nginx.sh
+++ copy/var/zoneinit/includes/46-nginx.sh
@@ -1,2 +1,8 @@
 # Enable nginx service
 svcadm enable svc:/pkgsrc/nginx:default
+
+# Configure SSL
+/opt/core/bin/ssl-generate.sh /opt/local/etc/nginx/ssl nginx_ssl nginx svc:/pkgsrc/nginx:default
+
+# Restart nginx to support new SSL certificate
+svcadm restart svc:/pkgsrc/nginx:default
